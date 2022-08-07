a2cd2a4baa568cabbe2f57ba75ca1c46d5631506
Aydoğan Gökberk <aydogan.gokberk@gmail.com>
2022-08-07 21:49:18 +0300

    copy to clipboard feature add

--- views/assets/scripts/main.js
+++ views/assets/scripts/main.js
@@ -4,13 +4,21 @@ const submit = document.querySelector("[data-submit]");
 const success = document.querySelector("[data-success]");
 
 /* Form validtion */
-form?.addEventListener("submit", (e) => {
- if (!(input.validity.valid && input.value.length > 10)) return
+form?.addEventListener("submit", e => {
+  if (!(input.validity.valid && input.value.length > 10)) return;
 
-    form.value = ""; // Clear the input
-    submit.setAttribute("aria-busy", "true")
+  form.value = ""; // Clear the input
+  submit.setAttribute("aria-busy", "true");
 });
 
 const params = new URLSearchParams(window.location.search);
 const shortID = params.get("success");
 
+const copy = async e => {
+  const text = e.getAttribute("data-url");
+  const url = `${window.location.origin}/${text}`;
+
+  await navigator.clipboard.writeText(url);
+  e.setAttribute("data-tooltip", "🎉 Copied!");
+  setTimeout(() => e.setAttribute("data-tooltip", "Copy to clipboard"), 5000);
+};
